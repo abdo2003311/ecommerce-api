30d8e931d748d75fd70f7cab5406df2b7bf70038
unknown <babelly1996@gmail.com>
2022-08-02 16:24:20 +0300

    deploying

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import { hashSync } from 'bcryptjs';
 import express from 'express';
 const app = express();
-import mongoose, { ConnectOptions } from 'mongoose';
+import { CATEGORIES, PRODUCTS, USERS } from './data';
+import mongoose from 'mongoose';
 import { urlencoded, json } from 'express';
 import cors from 'cors';
 require('dotenv').config();
@@ -19,109 +20,106 @@ app.use(cors());
 mongoose.connect(DATABASE,
     () => {
     console.log(`connected to ${DATABASE}`);
+    CATEGORIES.forEach(async (category) => {
+        try {
+            await Category.create({
+                value : category.value
+            });
+        } catch (e) {
+            console.log(e)
+        }
+    
+    });
+    
+    setTimeout(() => {
+        PRODUCTS.forEach(async (product) => {
+    
+            try {
+        
+                let category = await Category.findOne({
+                    value : product.category.value
+                })
+            
+                await Product.create({
+                    title : product.title,
+                    category : category,
+                    price : product.price,
+                    rate : product.rate,
+                    count : product.count,
+                    image : product.image,
+                    desc : product.desc
+                });
+            } catch (e) {
+        
+                console.log(e)
+        
+            }
+            
+        });
+        
+        
+    }, 500)
+    
+    setTimeout(() => {
+        USERS.forEach(async (user) => {
+        
+            try {
+        
+                let product = (products: string | any[]) => {
+                    return (
+                        {
+                            id : products[Math.floor(Math.random() * products.length)]._id,
+                            quantity : Math.ceil(Math.random() * 10),
+                        }
+                    )
+                }
+        
+                let products = await Product.find();
+        
+                let cart = await Cart.create({
+                    userId : null,
+                    products : [product(products), product(products), product(products)]
+                });
+            
+                let createdUser = await User.create({
+                    username : user.username,
+                    cart : cart,
+                    email : user.email,
+                    password : hashSync(user.password, 8),
+                    firstname : user.firstname,
+                    lastname : user.lastname,
+                    phone : user.phone,
+                    address : {
+                        city : user.address.city,
+                        street : user.address.street
+                    },
+                    role : user.role
+                });
+        
+                cart.userId = createdUser._id;
+        
+                await cart.save();
+        
+            } catch (e) {
+        
+                console.log(e)
+        
+            }
+            
+        });
+        
+    }, 1000)
 });
 
 // starting the server
 
 app.listen(PORT, () => console.log(`listening to port ${PORT}`));
 
-import { CATEGORIES, PRODUCTS, USERS } from './data';
 import { Cart, Category, Product, User } from './models';
 import { productsRouter, usersRouter } from './routers';
 import { adminAuthorization, getCarts, verifyToken } from './controllers';
 
 // creating database
-
-CATEGORIES.forEach(async (category) => {
-    try {
-        await Category.create({
-            value : category.value
-        });
-    } catch (e) {
-        console.log(e)
-    }
-
-});
-
-setTimeout(() => {
-    PRODUCTS.forEach(async (product) => {
-
-        try {
-    
-            let category = await Category.findOne({
-                value : product.category.value
-            })
-        
-            await Product.create({
-                title : product.title,
-                category : category,
-                price : product.price,
-                rate : product.rate,
-                count : product.count,
-                image : product.image,
-                desc : product.desc
-            });
-        } catch (e) {
-    
-            console.log(e)
-    
-        }
-     
-    });
-    
-    
-}, 500)
-
-setTimeout(() => {
-    USERS.forEach(async (user) => {
-    
-        try {
-    
-            let product = (products: string | any[]) => {
-                return (
-                    {
-                        id : products[Math.floor(Math.random() * products.length)]._id,
-                        quantity : Math.ceil(Math.random() * 10),
-                    }
-                )
-            }
-    
-            let products = await Product.find();
-    
-            let cart = await Cart.create({
-                userId : null,
-                products : [product(products), product(products), product(products)]
-            });
-        
-            let createdUser = await User.create({
-                username : user.username,
-                cart : cart,
-                email : user.email,
-                password : hashSync(user.password, 8),
-                firstname : user.firstname,
-                lastname : user.lastname,
-                phone : user.phone,
-                address : {
-                    city : user.address.city,
-                    street : user.address.street
-                },
-                role : user.role
-            });
-    
-            cart.userId = createdUser._id;
-    
-            await cart.save();
-    
-        } catch (e) {
-    
-            console.log(e)
-    
-        }
-     
-    });
-    
-}, 1000)
-
 
 
 app.use('/api/products/', productsRouter);
